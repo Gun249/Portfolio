5a2716acae4e6fd657ab6ed37953b3fd5046982f
Gun <gunthron14@gmail.com>
2025-07-08 15:40:21 +0700

    Refactor CSS styles for improved layout and responsiveness

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }));
+
+
+const copyright = document.getElementById("copyright");
+const currentYear = new Date().getFullYear();
+copyright.textContent = `© ${currentYear} Gunthorn. All rights reserved.`;
